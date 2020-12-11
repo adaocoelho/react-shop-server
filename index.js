d05d3a13186810892227f7f41f7c5465bb8781ff
adao <adaodcoelho@gmail.com>
2020-12-11 01:40:10 +0000

    sffffsss

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ if (process.env.NODE_ENV === "production") {
   // index.html for all page routes
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "dist"));
+    res.sendFile(path.join(__dirname + 'index.html'));
   });
 }
 
