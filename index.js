a8b6c7924fb874cb9a13c5a03279ca4f5146b9a8
adao <adaodcoelho@gmail.com>
2020-12-11 01:27:24 +0000

    sffff

--- index.js
+++ index.js
@@ -64,16 +64,16 @@ app.use("/", dialogFlowRoutes);
 const fulfillmentRoutes = require("./routes/fulfillmentRoutes");
 app.use("/", fulfillmentRoutes);
 
-/*if (process.env.NODE_ENV === "production") {
-  app.use(express.static("dist/build")); // js and css files
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("./dist/build")); // js and css files
   // index.html for all page routes
-  const path = require("path"); 
+  const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./dist/index.html"));
+    res.sendFile(path.join(__dirname, "dist"));
   });
-}*/
+}
 
-app.use("/", express.static(path.join(__dirname, "dist")));
+/*app.use("/", express.static(path.join(__dirname, "dist")));*/
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
